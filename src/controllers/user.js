266383c62e42679074db3701810419e808af6775
agustin mateos <agustinrotondomateos@gmail.com>
2023-06-28 21:23:01 -0300

    backend

---
+++ src/controllers/user.js
@@ -0,0 +1,12 @@
+import { findUsers } from "../services/UserServices.js";
+
+export const getUsers = async (req, res) => {
+    try {
+        const users = await findUsers()
+        res.status(200).send(users)
+
+    } catch (error) {
+        res.status(500).send(error)
+    }
+
+}
